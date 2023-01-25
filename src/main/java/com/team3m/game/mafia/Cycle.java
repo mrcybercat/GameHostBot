7e5190a6963e43214ce211e0304197cb225b493a
markondopolo <markondopolo@gmail.com>
2023-01-25 17:27:17 +0100

    feat - create Cycle.java
    Regarding this file we have a lot of things to implement which described in comments

---
+++ src/main/java/com/team3m/game/mafia/Cycle.java
@@ -0,0 +1,12 @@
+package com.team3m.game.mafia;
+
+public class Cycle {
+    /**
+     * this class controls the game cycle,
+     * stores players who are currently playing,
+     * receives all changes from some class that handles all changes
+     * stores all changes
+     * gives access to a list of alive players
+     * stores information about retired players
+     */
+}
