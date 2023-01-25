7e38ef89e08658444a5dbb35dc554a36f320358e
markondopolo <markondopolo@gmail.com>
2023-01-25 16:44:51 +0100

    feat - create Votes.java

---
+++ src/main/java/com/team3m/game/mafia/Votes.java
@@ -0,0 +1,40 @@
+package com.team3m.game.mafia;
+
+import java.util.ArrayList;
+
+public class Votes {
+
+    /**
+     * This class stores the voting mechanisms
+     * A data structure was created that stores players' votes
+     */
+
+
+    private final String voter_id;
+    private String target_id;
+
+    ArrayList<Votes> voteStorage = new ArrayList<>();
+
+    Votes(String target_id, String voter_id) {
+        this.target_id = target_id;
+        this.voter_id = voter_id;
+    }
+
+    void vote(String target_id, String voter_id) {
+        voteStorage.add(new Votes(target_id, voter_id));
+    }
+
+    void revote(String newTarget_id, String voter_id) {// search for the voter ID on the arrayList and change his vote to a new one
+        for (Votes votes : voteStorage) {
+            if (votes.voter_id.equals(voter_id)) {
+                votes.target_id = newTarget_id;
+            }
+
+        }
+    }
+
+    void cleanVoteStorage() {
+        voteStorage.clear();
+    }
+}
+
